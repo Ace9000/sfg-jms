801b37116db177bb9b22be44ffdc640d0c73b736
Anastas Acevski <anastas.acevski@outlook.com>
2023-05-05 12:36:51 +0200

    Initial commit

---
+++ src/test/java/springframework02/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework02.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
